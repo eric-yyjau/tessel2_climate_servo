8dfe7ae90395de147e941573b3df6a0f03b05a24
eric-yyjau <eric.yyjau@gmail.com>
2017-05-03 11:33:59 +0800

    update as chatroom example

--- index.js
+++ index.js
@@ -1,13 +1,115 @@
-// Import the interface to Tessel hardware
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var os = require('os');
+var path = require('path');
+var io = require('socket.io').listen(server),
+	nicknames = [];
+// climate
 var tessel = require('tessel');
+var climatelib = require('climate-si7020');
+var climate = climatelib.use(tessel.port['A']);
 
-// Turn one of the LEDs on to start.
-tessel.led[2].on();
+// servo
+var servolib = require('servo-pca9685');
+var servo = servolib.use(tessel.port['B']);
 
-// Blink!
-setInterval(function () {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
-}, 100);
+// var port = 8888;
+var port = 8000;
 
-console.log("I'm blinking! (Press CTRL + C to stop)");
+var av = require('tessel-av');
+// var camera = new av.Camera();
+var camera = new av.Camera({
+  dimensions: "160x120",
+  fps: 30
+});
+
+// climate 
+climate.on('ready', function () {
+  console.log('Connected to climate module');
+
+  // Loop forever
+  setImmediate(function loop () {
+    climate.readTemperature('f', function (err, temp) {
+      climate.readHumidity(function (err, humid) {
+      console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
+      setTimeout(loop, 300);
+      });
+    });
+  });
+});
+
+climate.on('error', function(err) {
+  console.log('error connecting module', err);
+});
+// climate end
+
+// servo
+var servo1 = 1; // We have a servo plugged in at position 1
+
+servo.on('ready', function () {
+  var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
+
+  //  Set the minimum and maximum duty cycle for servo 1.
+  //  If the servo doesn't move to its full extent or stalls out
+  //  and gets hot, try tuning these values (0.05 and 0.12).
+  //  Moving them towards each other = less movement range
+  //  Moving them apart = more range, more likely to stall and burn out
+  servo.configure(servo1, 0.05, 0.12, function () {
+    setInterval(function () {
+      console.log('Position (in range 0-1):', position);
+      //  Set servo #1 to position pos.
+      servo.move(servo1, position);
+
+      // Increment by 10% (~18 deg for a normal servo)
+      position += 0.1;
+      if (position > 1) {
+        position = 0; // Reset servo position
+      }
+    }, 2000); // Every 500 milliseconds
+  });
+});
+// servo end
+
+server.listen(port, function () {
+  console.log(`http://${os.hostname()}.local:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/stream', (request, response) => {
+  response.redirect(camera.url);
+});
+
+// ==== socket io ====
+
+io.sockets.on('connection', function(socket) {
+	socket.on('new user', function(data){
+		console.log(data);
+		if (nicknames.indexOf(data) != -1) {
+
+		} else {
+			socket.emit('chat', 'SERVER', '歡迎光臨 ' + data);
+
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			io.sockets.emit('usernames', nicknames);
+			updateNicknames();
+		}
+	});
+
+	function updateNicknames(){
+		io.sockets.emit('usernames', nicknames);
+	}
+
+	//
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', { msg: data, nick: socket.nickname });
+	});
+
+	socket.on('disconnect', function(data){
+		if (!socket.nickname) return;
+		io.sockets.emit('chat', 'SERVER', socket.nickname + ' 離開了聊天室～');
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		updateNicknames();
+	});
+});
